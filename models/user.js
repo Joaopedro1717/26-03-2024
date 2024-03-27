590047d6f6a61df8e33c40eaf14d43ef679eeca4
Joaopedro1717 <88109006+Joaopedro1717@users.noreply.github.com>
2024-03-26 21:49:16 -0300

    Add files via upload

---
+++ models/user.js
@@ -0,0 +1,52 @@
+// .models/user.js
+
+const Sequelize = require('sequelize');
+const { sequelize } = require('.');
+
+//Jeito do Escobar
+module.exports= (sequelize) => {
+    const User = sequelize.define('User',{
+        id:{
+            type: Sequelize.INTEGER,
+            primaryKey:true
+        },
+        nome:{
+            type: Sequelize.STRING,
+            allowNull:false,
+        },
+        email:{
+            type: Sequelize.STRING,
+            allowNull:false,
+            unique:true
+        },
+        senha:{
+            type: Sequelize.STRING,
+            allowNull: false,
+        }
+    });
+    return User;
+};
+
+const user = sequelize.define('User', {
+    id:{
+        type: Sequelize.INTEGER,
+        autoIncrement:true,
+        primaryKey:true,
+    },
+    nome:{
+        type: Sequelize.STRING,
+        allowNull:false,
+    },
+    email:{
+        type: Sequelize.STRING,
+        allowNull:false,
+        unique:true
+    },
+    senha:{
+        type: Sequelize.STRING,
+        allowNull: false,
+    }
+
+});
+
+module.exports = User;
